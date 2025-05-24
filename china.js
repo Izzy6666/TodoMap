7e04bc2a070716c456cd84419a0ef82c105d94f8
Izzy6666 <1345531839@qq.com>
2025-05-24 23:12:15 +0800

    china.js

---
+++ china.js
@@ -0,0 +1,19 @@
+var chinaJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {"type": "Feature","properties":{"name":"上海"},"geometry":{"type":"Polygon","coordinates":[[[121.0,31.1],[121.7,30.9],[121.6,31.4],[121.0,31.1]]]}},
+    {"type": "Feature","properties":{"name":"苏州"},"geometry":{"type":"Polygon","coordinates":[[[120.4,31.1],[120.8,30.9],[120.8,31.5],[120.4,31.1]]]}},
+    {"type": "Feature","properties":{"name":"无锡"},"geometry":{"type":"Polygon","coordinates":[[[120.1,31.5],[120.5,31.3],[120.5,31.8],[120.1,31.5]]]}},
+    {"type": "Feature","properties":{"name":"常州"},"geometry":{"type":"Polygon","coordinates":[[[119.6,31.6],[120.0,31.4],[120.0,31.9],[119.6,31.6]]]}},
+    {"type": "Feature","properties":{"name":"南京"},"geometry":{"type":"Polygon","coordinates":[[[118.6,32.0],[119.0,31.6],[119.0,32.3],[118.6,32.0]]]}},
+    {"type": "Feature","properties":{"name":"南通"},"geometry":{"type":"Polygon","coordinates":[[[120.7,32.0],[121.1,31.8],[121.1,32.3],[120.7,32.0]]]}},
+    {"type": "Feature","properties":{"name":"嘉兴"},"geometry":{"type":"Polygon","coordinates":[[[120.6,30.6],[121.0,30.4],[121.0,31.0],[120.6,30.6]]]}},
+    {"type": "Feature","properties":{"name":"宁波"},"geometry":{"type":"Polygon","coordinates":[[[121.3,29.9],[121.8,29.5],[121.8,30.2],[121.3,29.9]]]}},
+    {"type": "Feature","properties":{"name":"杭州"},"geometry":{"type":"Polygon","coordinates":[[[120.0,30.1],[120.6,29.8],[120.6,30.5],[120.0,30.1]]]}},
+    {"type": "Feature","properties":{"name":"绍兴"},"geometry":{"type":"Polygon","coordinates":[[[120.5,29.9],[121.0,29.7],[121.0,30.2],[120.5,29.9]]]}},
+    {"type": "Feature","properties":{"name":"湖州"},"geometry":{"type":"Polygon","coordinates":[[[120.0,30.8],[120.4,30.6],[120.4,31.1],[120.0,30.8]]]}},
+    {"type": "Feature","properties":{"name":"镇江"},"geometry":{"type":"Polygon","coordinates":[[[119.3,32.1],[119.7,31.8],[119.7,32.3],[119.3,32.1]]]}},
+    {"type": "Feature","properties":{"name":"扬州"},"geometry":{"type":"Polygon","coordinates":[[[119.2,32.5],[119.6,32.2],[119.6,32.7],[119.2,32.5]]]}},
+    {"type": "Feature","properties":{"name":"泰州"},"geometry":{"type":"Polygon","coordinates":[[[119.7,32.3],[120.1,32.1],[120.1,32.6],[119.7,32.3]]]}}
+  ]
+};
